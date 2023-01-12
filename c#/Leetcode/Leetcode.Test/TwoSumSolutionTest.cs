a0a768bb9af408acb714b9cb67b4280d7ae1e97e
Adrian Jarilla <adrian_jarilla@yahoo.com>
2023-01-13 05:04:11 +0800

    Added Solution and Unit Test for Two Sum problem

---
+++ c#/Leetcode/Leetcode.Test/TwoSumSolutionTest.cs
@@ -0,0 +1,53 @@
+﻿namespace Leetcode.Test
+{
+    [TestFixture]
+    public class TwoSumSolutionTest
+    {
+        private TwoSumSolution _twoSumSolution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _twoSumSolution = new TwoSumSolution();
+        }
+
+        [Test]
+        public void TwoSum_AscendingOrder_ReturnIndices()
+        {
+            var nums = new int[] { 1, 2, 3, 4 };
+            var target = 3;
+
+            var result = _twoSumSolution.TwoSum(nums, target);
+
+            Assert.That(result.Length, Is.EqualTo(2));
+            Assert.That(result[0], Is.EqualTo(0));
+            Assert.That(result[1], Is.EqualTo(1));
+
+        }
+
+        [Test]
+        public void TwoSum_UnOrdered_ReturnIndices()
+        {
+            var nums = new int[] { 5, 50, 75, 45 };
+            var target = 50;
+
+            var result = _twoSumSolution.TwoSum(nums, target);
+
+            Assert.That(result.Length, Is.EqualTo(2));
+            Assert.That(result[0], Is.EqualTo(0));
+            Assert.That(result[1], Is.EqualTo(3));
+        }
+
+        [Test] public void TwoSum_Duplicates_ReturnIndices()
+        {
+            var nums = new int[] { 1, 1, 4, 1,5 };
+            var target = 6;
+
+            var result = _twoSumSolution.TwoSum(nums, target);
+
+            Assert.That(result.Length, Is.EqualTo(2));
+            Assert.That(result[0], Is.EqualTo(3));
+            Assert.That(result[1], Is.EqualTo(4));
+        }
+    }
+}
---
+++ c#/Leetcode/Leetcode/TwoSumSolution.cs
@@ -0,0 +1,25 @@
+﻿namespace Leetcode
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var previousMap = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                var difference = target - num;
+
+                if (previousMap.ContainsKey(difference))
+                {
+                    return new int[] { previousMap[difference], i };
+                }
+
+                previousMap[num] = i;
+
+            }
+            return new int[] { };
+        }
+    }
+}
